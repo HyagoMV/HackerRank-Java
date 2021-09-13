fae70f885ad9f424c547a8eb3b215c55a6e0129b
Hyago <hyagomvale@gmail.com>
2021-09-13 15:01:04 -0300

    Solução do challenge: Matching Anything But a Newline
    https://www.hackerrank.com/challenges/matching-digits-non-digit-character/problem

---
+++ src/regex/Introduction/easy/Matching_Anything_But_a_Newline.java
@@ -0,0 +1,30 @@
+package regex.Introduction.easy;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/*
+ * Tarefa
+ *
+ * Você tem uma string S de teste.
+ * Sua tarefa é escrever uma expressão regular que corresponda apenas e exatamente a strings de form: abc.def.ghi.jkx, 
+ * 	onde cada variável pode ser qualquer caractere único, exceto a nova linha.
+ */
+public class Matching_Anything_But_a_Newline {
+
+	static {
+		try {
+			System.setIn(Matching_Anything_But_a_Newline.class.getResource("/Matching_Anything_But_a_Newline_STC0").openStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		try (var scanner = new Scanner(System.in)) {
+			System.out.println(Pattern.compile("(...\\.){3}...").matcher(scanner.nextLine()).matches());
+		}
+	}
+
+}

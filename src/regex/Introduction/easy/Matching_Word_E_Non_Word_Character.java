94f706c9ad02cae5b239747656007c679e1633ed
Hyago <hyagomvale@gmail.com>
2021-09-13 15:53:06 -0300

    Solução do challenge: Matching Word & Non-Word Character
    https://www.hackerrank.com/challenges/matching-word-non-word/problem

---
+++ src/regex/Introduction/easy/Matching_Word_E_Non_Word_Character.java
@@ -0,0 +1,30 @@
+package regex.Introduction.easy;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/*
+ * Tarefa
+ *
+ * Você tem uma string S de teste. 
+ * Sua tarefa é combinar o padrão xxxXxxxxxxxxxxXxxx
+ * Aqui x denota qualquer caractere de palavra e X qualquer caractere que não seja de palavra.
+ */
+public class Matching_Word_E_Non_Word_Character {
+
+	static {
+		try {
+			System.setIn(Matching_Word_E_Non_Word_Character.class.getResource("/Matching_Word_E_Non_Word_Character_STC0").openStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		try (var scanner = new Scanner(System.in)) {
+			System.out.println(Pattern.compile("\\w{3}\\W\\w{10}\\W\\w{3}").matcher(scanner.nextLine()).find());
+		}
+	}
+
+}
